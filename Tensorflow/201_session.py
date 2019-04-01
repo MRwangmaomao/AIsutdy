2c0284ff0a07a9ef5417b45ca5a2bcb96161020f
wangmaomao <wpr17@mails.tsinghua.edu.cn>
2019-04-01 19:37:52 +0800

    add all files

--- Tensorflow/201_session.py
+++ Tensorflow/201_session.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 m1 = tf.constant([[2, 2]])
 m2 = tf.constant([[3],
-                  [3]])
+      [3]])
 dot_operation = tf.matmul(m1, m2)
 
 print(dot_operation)  # wrong! no result
